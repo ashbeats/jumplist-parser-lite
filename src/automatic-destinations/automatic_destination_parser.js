048008f578553b311d73cf5923980175f5d39544
Ash <ashbeats@gmail.com>
2020-07-26 00:29:04 +0700

    support only buffer

--- src/automatic-destinations/automatic_destination_parser.js
+++ src/automatic-destinations/automatic_destination_parser.js
@@ -5,8 +5,12 @@ const CFB = require("cfb");
 
 const unpack_destlist_content = require("./unpack_destlist_content.js");
 
-function automatic_destination_parser(jmpPath) {
-  const cfb = CFB.read(jmpPath, { type: "file" });
+function automatic_destination_parser(bytes) {
+  
+  if (!(bytes instanceof Buffer)) throw "Input must be an instance of Buffer";
+  
+  const cfb = CFB.read(bytes, { type: "buffer" });
+  
   const DestList = CFB.find(cfb, "DestList");
   if (DestList && "content" in DestList) {
     return unpack_destlist_content(DestList.content);
